6338a42539e4622609d4029e759e47f3934a5586
Danil <boy4rov.da@gmail.com>
2024-11-29 21:16:08 +0300

    first commit

---
+++ cvservice/src/test/java/com/mreblan/cvservice/CvserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mreblan.cvservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
