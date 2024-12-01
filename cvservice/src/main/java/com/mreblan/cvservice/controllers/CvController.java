4d33f88bd5f5ce6ad642728ceedf5480544c87d5
Danil <boy4rov.da@gmail.com>
2024-12-01 18:20:27 +0300

    openAPI description added

--- cvservice/src/main/java/com/mreblan/cvservice/controllers/CvController.java
+++ cvservice/src/main/java/com/mreblan/cvservice/controllers/CvController.java
@@ -35,12 +35,21 @@ import com.mreblan.cvservice.services.FileService;
 import com.mreblan.cvservice.services.ZipService;
 import com.mreblan.cvservice.services.impl.TxtService;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import com.mreblan.cvservice.exceptions.CvsNotFoundException;
 import com.mreblan.cvservice.exceptions.CvAlreadyExistsException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@OpenAPIDefinition(info = @Info(title = "CV API", version = "v1"))
 @Slf4j
 @RequiredArgsConstructor
 @RestController
@@ -83,23 +92,41 @@ public class CvController {
     //     }
     // }
 
-    // @PostMapping("/extract")
-    // public ResponseEntity<String> extractTextFromFile(@RequestParam("file") MultipartFile file) {
-    //     String text = null;
-    //     try {
-    //         text = pdfService.extractText(file);
-    //         log.info("TEXT FROM FILE: {}", text);
-    //
-    //         return ResponseEntity.ok("Text extracted");
-    //     } catch (IOException e) {
-    //         log.error(e.getMessage());
-    //
-    //         return ResponseEntity
-    //                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
-    //                     .body("Something went wrong with extracting");
-    //     }
-    // }
 
+    @Operation(summary = "Сохранение резюме",
+        description = "Сохраняет текст из резюме в БД"
+    )
+    @ApiResponses(
+        value = {
+            @ApiResponse(responseCode = "200",
+            description = "Резюме успешно добавлено",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(
+                        example = "{ \"success\": true, \"description\": \"Резюме получено\"}"
+                    )
+                )
+            ),
+            @ApiResponse(responseCode = "400",
+            description = "Отсутствует файл или у него не тот формат",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(
+                        example = "{ \"success\": false, \"description\": \"Файл не загружен или не является файлом формата pdf\"}"
+                    )
+                )
+            ),
+            @ApiResponse(responseCode = "500",
+            description = "При сохранении БД что-то пошло не так",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(
+                        example = "{ \"success\": false, \"description\": \"Что-то пошло не так\"}"
+                    )
+                )
+            )
+        }
+    )
     @PostMapping("/saveCv")
     public ResponseEntity<Response> uploadCv(@RequestParam("file") MultipartFile file) {
         String text = null;
@@ -135,6 +162,25 @@ public class CvController {
         return createResponse(HttpStatus.INTERNAL_SERVER_ERROR, new Response(false, "Что-то пошло не так"));
     }
     
+    @Operation(summary = "Ищет резюме по ключевым словам",
+        description = "Ищет резюме, в которых присутствуют ключевые слова, указанные в запросе. Возвращает zip-архив"
+    )
+    @ApiResponses(
+        value = {
+            @ApiResponse(responseCode = "200",
+            description = "Резюме найдены",
+            content = @Content(
+                    mediaType = "application/zip",
+                    schema = @Schema(
+                        type = "string", format = "binary"
+                    )
+                )
+            ),
+            @ApiResponse(responseCode = "500",
+            description = "При формировании архива что-то пошло не так"
+            )
+        }
+    )
     @GetMapping("/search")
     public ResponseEntity<byte[]> searchByKeywords(@RequestBody FindByKeywordsRequest request) {
         List<CvModel> cvs = cvService.getCvByKeyword(request.getKeywords());
