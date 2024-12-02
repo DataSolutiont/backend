85863fa97502c6db5ea7375a4d44170012230ad2
Danil <boy4rov.da@gmail.com>
2024-12-02 20:38:46 +0300

    strict and weak search added

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-docker-compose -f ./cvservice//docker-compose.yaml up -d
-docker-compose -f ./auth/docker-compose.yaml up -d
-docker-compose -f ./gateway/docker-compose.yaml up -d
+docker compose -f ./cvservice//docker-compose.yaml up -d
+docker compose -f ./auth/docker-compose.yaml up -d
+docker compose -f ./gateway/docker-compose.yaml up -d
--- stop_all.sh
+++ stop_all.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-docker-compose -f ./auth/docker-compose.yaml down
-docker-compose -f ./cvservice//docker-compose.yaml down
-docker-compose -f ./gateway/docker-compose.yaml down
+docker compose -f ./auth/docker-compose.yaml down
+docker compose -f ./cvservice//docker-compose.yaml down
+docker compose -f ./gateway/docker-compose.yaml down
 
